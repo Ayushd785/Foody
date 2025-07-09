c54837f1920a498c2a1f33cda899ce1a361f4c37
Ayushd785 <ayushd785@gmail.com>
2025-07-09 11:45:36 +0530

    Added cart to database

---
+++ backend/models/Cart.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const cartSchema = mongoose.Schema({
+  customerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  items: [
+    {
+      menuItem: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Menu",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        default: 1,
+      },
+    },
+  ],
+});
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
